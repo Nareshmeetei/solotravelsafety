0fccd9c786960539b061b987c980c0aa316c81eb
.\.bash_history <meetei.naresh@gmail.com>
2025-09-02 13:54:36 +0530

    videos are fixed

--- src/pages/DestinationDetail.tsx
+++ src/pages/DestinationDetail.tsx
@@ -1271,7 +1271,7 @@ const DestinationDetail: React.FC = () => {
                         </h3>
                         <div className="relative w-full" style={{ paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
                           <iframe
-                            src={`https://www.youtube.com/embed/${dest.youtubeVideo.videoId}`}
+                            src={`https://www.youtube-nocookie.com/embed/${dest.youtubeVideo.videoId}`}
                             title="YouTube video player"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
